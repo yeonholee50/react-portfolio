b345fe5ad8c256002d6be0600ce8fd968fbc8ec5
Yeon Lee <yeonholee50@gmail.com>
2025-01-23 19:25:38 -0500

    completed portfolio

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,27 +1,61 @@
-import Project1 from "../../assets/project1.webp";
-import Project2 from "../../assets/project2.webp";
-import Project3 from "../../assets/project3.webp";
+import Project1 from "../../assets/ampyfin.webp";
+import Project2 from "../../assets/nyxhub.webp";
+import Project3 from "../../assets/leetcodetwitter.webp";
+import Project4 from "../../assets/minidog.webp";
+import Project5 from "../../assets/jin.webp";
+import Project6 from "../../assets/intro.webp";
 
 
 export const projectsData = [
     {
         id: 1,
         image: Project1,
-        title: "Project 1",
-        category: "A",
+        link: "https://ampyfin.shop/",
+        title: "AmpyFin: Ranked Ensemble Learning Trading Bot",
+        category: "AmpyFin",
+        github: "https://github.com/yeonholee50/AmpyFin",
     },
     {
         id: 2,
         image: Project2,
-        title: "Project 2",
-        category: "B",
+        link: "https://nyxhub.shop/",
+        title: "NyxHub: Secure Web File Sharing Application",
+        category: "NyxHub",
+        github: "https://github.com/yeonholee50/NyxHub",
     },
     {
         id: 3,
         image: Project3,
-        title: "Project 3",
-        category: "C",
+        link: "https://leetcodetwitter.onrender.com/",
+        title: "LeetCode Twitter: Social Media Site Inspired By LeetCode Problem",
+        category: "LeetCode Twitter",
+        github: "https://github.com/yeonholee50/LeetCodeTwitter",
     },
+
+    {
+        id: 4,
+        image: Project4,
+        link: "https://youtu.be/1oapv1quKkc",
+        title: "MiniDog: Miniature Luggage Carrying Robot",
+        category: "MiniDog",
+        github: "https://github.com/yeonholee50/MiniDog",
+    },
+    {
+        id: 5,
+        image: Project5,
+        link: "https://slackbot-5i97.onrender.com/",
+        title: "Jin: A Slack Bot To Help With Workflow Efficiency",
+        category: "Jin",
+        github: "https://github.com/yeonholee50/Jin",
+    },
+    {
+        id: 6,
+        image: Project6,
+        link: "https://github.com/yeonholee50/Platform-Game",
+        title: "Platform Game using GBA Emulator",
+        category: "Platform Game: DX Ball",
+        github: "https://github.com/yeonholee50/Platform-Game",
+    }
 ];
 
 export const projectsNav = [
@@ -29,12 +63,23 @@ export const projectsNav = [
         name: "All",
     },
     {
-        name: "A",
+        name: "AmpyFin",
     },
     {
-        name: "B",
+        name: "NyxHub",
     },
     {
-        name: "C",
+        name: "LeetCode Twitter",
     },
+    {
+        name: "MiniDog",
+    },
+    {
+        name: "Jin",
+    },
+    {
+        name: "Platform Game: DX Ball",
+    },
+    
+
 ];
--- src/components/projects/ProjectItems.jsx
+++ src/components/projects/ProjectItems.jsx
@@ -6,9 +6,13 @@ const ProjectItems = ({item}) => {
         <div className="project__card" key={item.id}>
             <img className="project__img" src={item.image} alt="" />
             <h3 className="project__title">{item.title}</h3>
-            <a href="#" className="project__button">
+            <a href={item.link} className="project__button">
                 Demo <HiOutlineArrowSmRight className="project__button-icon" />
             </a>
+            <a href={item.github} className="project__button">
+                GitHub <HiOutlineArrowSmRight className="project__button-icon" />
+            </a>
+            
         </div>
     );
 }
