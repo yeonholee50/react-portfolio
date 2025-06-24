ca0b20a4531c9e272f49fb7034bfd21b2389bf2d
Yeon Lee <yeon.lee@attachmentsking.com>
2025-06-23 21:57:52 -0700

    f

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -1634,28 +1634,28 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "June 21, 2025",
-        effective: "June 22, 2025"
+        signed: "June 23, 2025",
+        effective: "June 23, 2025"
       },
       physical: {
-        signed: "June 21, 2025",
-        effective: "June 22, 2025"
+        signed: "June 23, 2025",
+        effective: "June 23, 2025"
       },
       career: {
-        signed: "June 21, 2025",
-        effective: "June 22, 2025"
+        signed: "June 23, 2025",
+        effective: "June 23, 2025"
       },
       financial: {
-        signed: "June 21, 2025",
-        effective: "June 22, 2025"
+        signed: "June 23, 2025",
+        effective: "June 23, 2025"
       },
       ampyfin: {
-        signed: "June 21, 2025",
-        effective: "June 22, 2025"
+        signed: "June 23, 2025",
+        effective: "June 23, 2025"
       },
       workout: {
-        signed: "June 21, 2025",
-        effective: "June 22, 2025"
+        signed: "June 23, 2025",
+        effective: "June 23, 2025"
       }
     };
 
