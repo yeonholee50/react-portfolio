5c2e5fa2fdbb7d7c33a3bfb6494cbb459840351a
Yeon Lee <yeonlee@Yeons-MacBook-Pro.local>
2025-07-04 19:07:44 -0700

    f

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -1634,28 +1634,28 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "July 3, 2025 at 8:00 PM PST",
-        effective: "July 3, 2025 at 8:00 PM PST"
+        signed: "July 4, 2025 at 10:00 PM PST",
+        effective: "July 4, 2025 at 10:00 PM PST"
       },
       physical: {
-        signed: "July 3, 2025 at 8:00 PM PST",
-        effective: "July 3, 2025 at 8:00 PM PST"
+        signed: "July 4, 2025 at 10:00 PM PST",
+        effective: "July 4, 2025 at 10:00 PM PST"
       },
       career: {
-        signed: "July 3, 2025 at 8:00 PM PST",
-        effective: "July 3, 2025 at 8:00 PM PST"
+        signed: "July 4, 2025 at 10:00 PM PST",
+        effective: "July 4, 2025 at 10:00 PM PST"
       },
       financial: {
-        signed: "July 3, 2025 at 8:00 PM PST",
-        effective: "July 3, 2025 at 8:00 PM PST"
+        signed: "July 4, 2025 at 10:00 PM PST",
+        effective: "July 4, 2025 at 10:00 PM PST"
       },
       ampyfin: {
-        signed: "July 3, 2025 at 8:00 PM PST",
-        effective: "July 3, 2025 at 8:00 PM PST"
+        signed: "July 4, 2025 at 10:00 PM PST",
+        effective: "July 4, 2025 at 10:00 PM PST"
       },
       workout: {
-        signed: "July 3, 2025 at 8:00 PM PST",
-        effective: "July 3, 2025 at 8:00 PM PST"
+        signed: "July 4, 2025 at 10:00 PM PST",
+        effective: "July 4, 2025 at 10:00 PM PST"
       }
     };
 
