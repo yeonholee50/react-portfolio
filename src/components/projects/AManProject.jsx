bc68b49e6f1e8806e41b20e7519b0959e34f9a23
Yeon Lee <yeonholee50@gmail.com>
2025-07-07 21:08:52 -0700

    f

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -1641,28 +1641,28 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "July 7, 2025 at 8:00 PM PST",
-        effective: "July 7, 2025 at 8:00 PM PST"
+        signed: "July 7, 2025 at 10:00 PM PST",
+        effective: "July 7, 2025 at 10:00 PM PST"
       },
       physical: {
-        signed: "July 7, 2025 at 8:00 PM PST",
-        effective: "July 7, 2025 at 8:00 PM PST"
+        signed: "July 7, 2025 at 10:00 PM PST",
+        effective: "July 7, 2025 at 10:00 PM PST"
       },
       career: {
-        signed: "July 7, 2025 at 8:00 PM PST",
-        effective: "July 7, 2025 at 8:00 PM PST"
+        signed: "July 7, 2025 at 10:00 PM PST",
+        effective: "July 7, 2025 at 10:00 PM PST"
       },
       financial: {
-        signed: "July 7, 2025 at 8:00 PM PST",
-        effective: "July 7, 2025 at 8:00 PM PST"
+        signed: "July 7, 2025 at 10:00 PM PST",
+        effective: "July 7, 2025 at 10:00 PM PST"
       },
       ampyfin: {
-        signed: "July 7, 2025 at 8:00 PM PST",
-        effective: "July 7, 2025 at 8:00 PM PST"
+        signed: "July 7, 2025 at 10:00 PM PST",
+        effective: "July 7, 2025 at 10:00 PM PST"
       },
       workout: {
-        signed: "July 7, 2025 at 8:00 PM PST",
-        effective: "July 7, 2025 at 8:00 PM PST"
+        signed: "July 7, 2025 at 10:00 PM PST",
+        effective: "July 7, 2025 at 10:00 PM PST"
       }
     };
 
