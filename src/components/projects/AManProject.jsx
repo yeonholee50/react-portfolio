cffcac40c09cc68cda8826a35c185eff09675c4b
Yeon Lee <yeonholee50@gmail.com>
2025-07-29 07:22:07 -0700

    f

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -1515,28 +1515,28 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "July 28, 2025 at 10:00 PM PDT",
-        effective: "July 28, 2025 at 10:00 PM PDT"
+        signed: "July 29, 2025 at 10:00 PM PDT",
+        effective: "July 29, 2025 at 10:00 PM PDT"
       },
       physical: {
-        signed: "July 28, 2025 at 10:00 PM PDT",
-        effective: "July 28, 2025 at 10:00 PM PDT"
+        signed: "July 29, 2025 at 10:00 PM PDT",
+        effective: "July 29, 2025 at 10:00 PM PDT"
       },
       career: {
-        signed: "July 28, 2025 at 10:00 PM PDT",
-        effective: "July 28, 2025 at 10:00 PM PDT"
+        signed: "July 29, 2025 at 10:00 PM PDT",
+        effective: "July 29, 2025 at 10:00 PM PDT"
       },
       financial: {
-        signed: "July 28, 2025 at 10:00 PM PDT",
-        effective: "July 28, 2025 at 10:00 PM PDT"
+        signed: "July 29, 2025 at 10:00 PM PDT",
+        effective: "July 29, 2025 at 10:00 PM PDT"
       },
       ampyfin: {
-        signed: "July 28, 2025 at 10:00 PM PDT",
-        effective: "July 28, 2025 at 10:00 PM PDT"
+        signed: "July 29, 2025 at 10:00 PM PDT",
+        effective: "July 29, 2025 at 10:00 PM PDT"
       },
       workout: {
-        signed: "July 28, 2025 at 10:00 PM PDT",
-        effective: "July 28, 2025 at 10:00 PM PDT"
+        signed: "July 29, 2025 at 10:00 PM PDT",
+        effective: "July 29, 2025 at 10:00 PM PDT"
       }
     };
 
