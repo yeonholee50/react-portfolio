915bcf500a96f2c40adb1e756cc8cd6b6a8cc87e
Yeon Lee <yeonholee50@gmail.com>
2025-09-23 19:36:46 -0400

    gf

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -1327,28 +1327,28 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "September 22, 2025 at 10:40 PM EST",
-        effective: "September 22, 2025 at 10:40 PM EST"
+        signed: "September 23, 2025 at 10:40 PM EST",
+        effective: "September 23, 2025 at 10:40 PM EST"
       },
       physical: {
-        signed: "September 22, 2025 at 10:40 PM EST",
-        effective: "September 22, 2025 at 10:40 PM EST"
+        signed: "September 23, 2025 at 10:40 PM EST",
+        effective: "September 23, 2025 at 10:40 PM EST"
       },
       career: {
-        signed: "September 22, 2025 at 10:40 PM EST",
-        effective: "September 22, 2025 at 10:40 PM EST"
+        signed: "September 23, 2025 at 10:40 PM EST",
+        effective: "September 23, 2025 at 10:40 PM EST"
       },
       financial: {
-        signed: "September 22, 2025 at 10:40 PM EST",
-        effective: "September 22, 2025 at 10:40 PM EST"
+        signed: "September 23, 2025 at 10:40 PM EST",
+        effective: "September 23, 2025 at 10:40 PM EST"
       },
       ampyfin: {
-        signed: "September 22, 2025 at 10:40 PM EST",
-        effective: "September 22, 2025 at 10:40 PM EST"
+        signed: "September 23, 2025 at 10:40 PM EST",
+        effective: "September 23, 2025 at 10:40 PM EST"
       },
       workout: {
-        signed: "September 22, 2025 at 10:40 PM EST",
-        effective: "September 22, 2025 at 10:40 PM EST"
+        signed: "September 23, 2025 at 10:40 PM EST",
+        effective: "September 23, 2025 at 10:40 PM EST"
       }
     };
 
