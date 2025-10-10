aa040c763ba418252d7914a108a522ab1604974c
Yeon Lee <yeonholee50@gmail.com>
2025-10-10 10:11:00 -0400

    f

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -2121,32 +2121,32 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "October 10, 2025 at 12:40 AM EST",
-        effective: "October 10, 2025 at 12:40 AM EST"
+        signed: "October 10, 2025 at 10:00 PM EST",
+        effective: "October 10, 2025 at 10:00 PM EST"
       },
       physical: {
-        signed: "October 10, 2025 at 12:40 AM EST",
-        effective: "October 10, 2025 at 12:40 AM EST"
+        signed: "October 10, 2025 at 10:00 PM EST",
+        effective: "October 10, 2025 at 10:00 PM EST"
       },
       mental: {
-        signed: "October 10, 2025 at 12:40 AM EST",
-        effective: "October 10, 2025 at 12:40 AM EST"
+        signed: "October 10, 2025 at 10:00 PM EST",
+        effective: "October 10, 2025 at 10:00 PM EST"
       },
       financial: {
-        signed: "October 10, 2025 at 12:40 AM EST",
-        effective: "October 10, 2025 at 12:40 AM EST"
+        signed: "October 10, 2025 at 10:00 PM EST",
+        effective: "October 10, 2025 at 10:00 PM EST"
       },
       career: {
-        signed: "October 10, 2025 at 12:40 AM EST",
-        effective: "October 10, 2025 at 12:40 AM EST"
+        signed: "October 10, 2025 at 10:00 PM EST",
+        effective: "October 10, 2025 at 10:00 PM EST"
       },
       'daily-os': {
-        signed: "October 10, 2025 at 12:40 AM EST",
-        effective: "October 10, 2025 at 12:40 AM EST"
+        signed: "October 10, 2025 at 10:00 PM EST",
+        effective: "October 10, 2025 at 10:00 PM EST"
       },
       workout: {
-        signed: "October 10, 2025 at 12:40 AM EST",
-        effective: "October 10, 2025 at 12:40 AM EST"
+        signed: "October 10, 2025 at 10:00 PM EST",
+        effective: "October 10, 2025 at 10:00 PM EST"
       }
     };
 
