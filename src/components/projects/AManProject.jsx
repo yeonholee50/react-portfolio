7af97b3a881644ec3bd476db7830de281142cde3
Yeon Lee <yeonholee50@gmail.com>
2025-10-21 12:04:53 -0400

    final

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -2991,32 +2991,32 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "October 20, 2025 at 10:00 PM EST",
-        effective: "October 20, 2025 at 10:00 PM EST"
+        signed: "October 21, 2025 at 10:00 PM EST",
+        effective: "October 21, 2025 at 10:00 PM EST"
       },
       physical: {
-        signed: "October 20, 2025 at 10:00 PM EST",
-        effective: "October 20, 2025 at 10:00 PM EST"
+        signed: "October 21, 2025 at 10:00 PM EST",
+        effective: "October 21, 2025 at 10:00 PM EST"
       },
       mental: {
-        signed: "October 20, 2025 at 10:00 PM EST",
-        effective: "October 20, 2025 at 10:00 PM EST"
+        signed: "October 21, 2025 at 10:00 PM EST",
+        effective: "October 21, 2025 at 10:00 PM EST"
       },
       financial: {
-        signed: "October 20, 2025 at 10:00 PM EST",
-        effective: "October 20, 2025 at 10:00 PM EST"
+        signed: "October 21, 2025 at 10:00 PM EST",
+        effective: "October 21, 2025 at 10:00 PM EST"
       },
       career: {
-        signed: "October 20, 2025 at 10:00 PM EST",
-        effective: "October 20, 2025 at 10:00 PM EST"
+        signed: "October 21, 2025 at 10:00 PM EST",
+        effective: "October 21, 2025 at 10:00 PM EST"
       },
       'daily-os': {
-        signed: "October 20, 2025 at 10:00 PM EST",
-        effective: "October 20, 2025 at 10:00 PM EST"
+        signed: "October 21, 2025 at 10:00 PM EST",
+        effective: "October 21, 2025 at 10:00 PM EST"
       },
       workout: {
-        signed: "October 20, 2025 at 10:00 PM EST",
-        effective: "October 20, 2025 at 10:00 PM EST"
+        signed: "October 21, 2025 at 10:00 PM EST",
+        effective: "October 21, 2025 at 10:00 PM EST"
       }
     };
 
