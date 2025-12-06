5a4a237bbf565e29e33e594e9c1baec127015f95
Yeon Lee <yeonholee50@gmail.com>
2025-12-06 09:40:05 -0500

    final

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -3117,36 +3117,36 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       },
       physical: {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       },
       mental: {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       },
       financial: {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       },
       career: {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       },
       'daily-os': {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       },
       workout: {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       },
       ampyfin: {
-        signed: "December 5, 2025 at 10:00 PM EST",
-        effective: "December 5, 2025 at 10:00 PM EST"
+        signed: "December 6, 2025 at 10:00 PM EST",
+        effective: "December 6, 2025 at 10:00 PM EST"
       }
     };
 
