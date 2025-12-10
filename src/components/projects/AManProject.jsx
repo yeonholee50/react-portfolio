a2b783712c587ee689238779ab9bed2877daf237
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2025-12-10 08:24:51 -0500

    Update AManProject.jsx

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -3117,36 +3117,36 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       },
       physical: {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       },
       mental: {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       },
       financial: {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       },
       career: {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       },
       'daily-os': {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       },
       workout: {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       },
       ampyfin: {
-        signed: "December 9, 2025 at 10:00 PM EST",
-        effective: "December 9, 2025 at 10:00 PM EST"
+        signed: "December 10, 2025 at 10:00 PM EST",
+        effective: "December 10, 2025 at 10:00 PM EST"
       }
     };
 
