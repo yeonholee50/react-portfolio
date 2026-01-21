5240f66b352a0820d8a7da47fd983ce13c45ab88
Yeon Lee <yeonholee50@gmail.com>
2026-01-20 19:58:03 -0500

    Update A-Man Project dates to January 20, 2026 at 10:00 PM

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -3043,36 +3043,36 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       },
       physical: {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       },
       mental: {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       },
       financial: {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       },
       career: {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       },
       'daily-os': {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       },
       workout: {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       },
       ampyfin: {
-        signed: "January 19, 2026 at 10:00 PM",
-        effective: "January 19, 2026 at 10:00 PM"
+        signed: "January 20, 2026 at 10:00 PM",
+        effective: "January 20, 2026 at 10:00 PM"
       }
     };
 
