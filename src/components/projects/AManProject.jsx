1d31fdb6f6b64e8446c40059eadacb3fa442b573
Yeon Lee <yeonholee50@gmail.com>
2026-03-18 19:34:25 -0700

    Update A-Man project dates to March 18, 2026 at 10:00 PM

--- src/components/projects/AManProject.jsx
+++ src/components/projects/AManProject.jsx
@@ -2912,36 +2912,36 @@ const AManProject = () => {
   const renderSignature = () => {
     const dates = {
       social: {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       },
       physical: {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       },
       mental: {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       },
       financial: {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       },
       career: {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       },
       'daily-os': {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       },
       workout: {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       },
       ampyfin: {
-        signed: "March 16, 2026 at 11:50 PM",
-        effective: "March 16, 2026 at 11:50 PM"
+        signed: "March 18, 2026 at 10:00 PM",
+        effective: "March 18, 2026 at 10:00 PM"
       }
     };
 
